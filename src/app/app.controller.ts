baec3d96463f552b4350deed081bb7f190180ad1
yankyawmoe <yankyawmoe599@gmail.com>
2021-11-26 15:03:57 +0630

    jwt authentication

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,19 +1,22 @@
 import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
-import { AuthenticatedGuard } from '../auth/authenticated.guard';
+// import { AuthenticatedGuard } from '../auth/authenticated.guard';
 import { LocalAuthGuard } from '../auth/local-auth.guard';
+import { AuthService } from '../auth/auth.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
+    constructor(private authService: AuthService){}
 
     @UseGuards(LocalAuthGuard)
     @Post('/login')
     login(@Request() req): any {
-        return {msg: 'Logged in!'};
+        return this.authService.login(req.user);
     }
 
-    @UseGuards(AuthenticatedGuard)
-    @Get('/protected')
-    getHello(@Request() req): any {
+    @UseGuards(JwtAuthGuard)
+    @Get('/profile')
+    getProfile(@Request() req): any {
         return req.user;
     }
 }
